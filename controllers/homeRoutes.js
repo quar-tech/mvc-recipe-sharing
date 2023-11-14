0c1e4c866c1554c2a94751df4883ff2a6222e6df
SolangeI <solange.indaco@gmail.com>
2023-11-13 20:57:42 -0800

    Fix a bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -60,8 +60,6 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    console.log(user);
-
     res.render('profile', {
       ...user,
       logged_in: true
